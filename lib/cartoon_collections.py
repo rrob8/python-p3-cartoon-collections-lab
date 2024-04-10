2b4ee869def9c8c176c76be9881fafb578dd15fc
Robert Bubnik <rbubnik@gmail.com>
2024-04-10 15:07:34 -0400

    passed tests

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,25 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for dwarf in dwarves:
+        print(f'{dwarves.index(dwarf)+1}. {dwarf}')
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteers):
+    calls = []
+    for element in planeteers:
+        calls.append(f"{element.capitalize()}!")
+    return calls
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(list):
+    value = ''
+    for word in list:
+        if len(word) > 4:
+            value = True
+            break
+        else:
+            value = False
 
-def find_the_cheese():
-    pass
+    return value
+
+def find_the_cheese(strings):
+    for item in strings:
+        if item == 'gouda' or item ==  'cheddar' or  item == 'camembert':
+            return item
